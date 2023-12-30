4fef729be651ebc6fdf647def2538d2ac75c6e38
unknown <usenavigate@gmail.com>
2023-12-30 14:45:46 -0500

    using alias

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -1,12 +1,29 @@
-describe('forms test', () => {
-    beforeEach(()=>{
-        cy.visit("/forms")
-    })
-    it('Test subscribe from',()=>{
-        cy.contains(/Testing Forms/i).should('be.visible')
-        cy.getDataTest('subscribe-form').find('input').type('ryan@coderyan.com')
-        cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('not.exist')
-        cy.getDataTest("subscribe-button").click()
-        cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('exist')
-    })
-})
+describe("forms test", () => {
+  beforeEach(() => {
+    cy.visit("/forms");
+  });
+  it("Test subscribe from", () => {
+    //using alias
+    cy.contains(/Testing Forms/i);
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+    cy.get("@subscribe-input").type("ryan@coderyan.com");
+    cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should("not.exist");
+
+    //putting invalid email to see that it doesnt work
+    cy.get("@subscribe-input").type("ryan@coderyan.io");
+    cy.contains(/Invalid email: ryan@coderyan.io!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Invalid email: ryan@coderyan.io!/i).should("exist");
+
+    cy.wait(3000)
+    cy.contains(/Invalid email: ryan@coderyan.io!/i).should("not.exist");
+    //testing complete failure: by not putting any input values in the form
+    cy.contains(/fail!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/fail!/i).should("exist");
+  });
+});
